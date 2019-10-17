3741d9bf6e2bd34040ec49aeb4bbc836886b95a3
meghana1210 <56668278+meghana1210@users.noreply.github.com>
2019-10-17 11:23:15 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,19 @@
+<?php  
+$a1 = Array('0' => Array('0' => 1,'1' => 2),'1' => Array('0' => 4,'1' => 5)); 
+ 
+ 
+$a2 = Array('0' => Array('0' => 1,'1' => 2),'1' => Array('0' => 4,'1' => 5)); 
+ 
+$result = array(); 
+for($i=0; $i<=1; $i++) { 
+for($j=0; $j<=1; $j++) { 
+$result[$i][$j] = $a1[$i][$j] + $a2[$i][$j]; 
+} 
+} 
+echo "<p> Matrix one </p>"; 
+echo "<pre/>";print_r($a1); 
+echo "<p> Matrix Two</p>"; 
+echo "<pre/>";print_r($a2); 
+echo "<p> Matrix Addition</p>"; 
+echo "<pre/>";print_r($result); 
+?> 
